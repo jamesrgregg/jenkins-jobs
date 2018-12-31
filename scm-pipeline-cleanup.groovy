ca9a246bbdd107d43fabfd56d0fbc85bbbc24324
James Gregg <james.r.gregg@intel.com>
2018-12-31 10:04:17 -0700

    update local pipeline cleanup and edgex-device-grove-c jjb
    Signed-off-by: James Gregg <james.r.gregg@intel.com>

--- scm-pipeline-cleanup.groovy
+++ scm-pipeline-cleanup.groovy
@@ -12,29 +12,33 @@ podTemplate(label: 'mypod', containers: [
   stage('Check running containers') {
    container('docker') {
     // example to show you can run docker commands when you mount the socket
+    sh 'echo list all of the images inside Jenkins'
     sh 'docker images'
     sh 'docker images -f dangling=true'
+    sh 'echo list all of the containers inside Jenkins'
     sh 'docker ps'
-    //sh 'docker ps -a -f status=exited'
-    //sh 'docker rm detached-nginx'
-    //sh 'docker rm docker-nonroot-nginx'
-
+    sh 'docker ps -a -f status=exited'
    }
   }
   stage('Starting Cleanup ') {
-   container('compose') {
+   container('docker') {
     dir('.') {
+     // change container to compose image if cleaning up via docker-compose
      // step([$class: 'DockerComposeBuilder', dockerComposeFile: 'docker-compose.yml', option: [$class: 'ExecuteCommandInsideContainer', command: 'pull', index: 1, privilegedMode: false, service: 'volume', workDir: '.'], useCustomDockerComposeFile: true])
+     // sh 'docker-compose stop'
+     // sh 'docker-compose down'
      sh 'echo starting cleanup of all Docker images'
-     //sh 'docker-compose stop'
-     //sh 'docker-compose down'
+     sh 'echo disk space check before cleanup'
+     sh 'docker system df'
+     sh 'docker rm $(docker ps -a -f status=exited) || docker rmi $(docker images -q -f dangling=true)'
     }
-    stage('Cleanup images') {
+    stage('Cleanup Images') {
      container('docker') {
       dir('.') {
        //sh 'docker rmi docker-nonroot-nginx --force'
-       sh 'docker rmi nginx'
+       sh 'docker rmi device-grove-c'
        sh 'docker images'
+       sh 'echo disk space check after cleanup'
        sh 'docker system df'
       }
      }
